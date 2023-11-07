aeda41896e85fcc5f9e47afc930d034440586a8e
Xi Yu <30382875+Cecilia831@users.noreply.github.com>
2023-11-07 13:20:15 -0800

    Read .xlsx

--- Main.cs
+++ Main.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using GemBox.Spreadsheet;
 
 namespace Automate
 {
@@ -11,7 +12,16 @@ namespace Automate
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hi!");
+
+            SpreadsheetInfo.SetLicense("FREE-LIMITED-KEY");
+
+            ExcelFile workbook = new ExcelFile();
+            ExcelWorksheet worksheet = workbook.Worksheets.Add("Sheet1");
+            ExcelCell cell = worksheet.Cells["A1"];
+
+            cell.Value = "Hello World!";
+
+            workbook.Save("HelloWorld.xlsx");
         } 
     }
 }
